a4118aa5d240ea7cbb2b21204b4fbb486c30f4f4
Masumi Takada <masumi.takada@gmail.com>
2017-12-18 13:19:04 +0000

    first

---
+++ database/migrations/2017_12_18_125342_create_people_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePeopleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->increments('id');
+            //故人の名前
+            $table->string('firstName');
+            $table->string('secondName');
+            //施主の名前
+            $table->string('ownerFirstName');
+            $table->string('ownerSecondName');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+}
